a526ceb4c5c9933c8be9105bf6dd674f164ef933
Yulei Liu <yulei.liu@gmail.com>
2019-08-03 22:53:51 +1000

    first try

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "gitbucket_repo_module" {
+  source  = "app.terraform.io/SpecialCustomer/gitbucket-repo-module/specialcustomer"
+  version = "1.3.9"
+
+  reponame = "${var.projectname}"
+}
+
+module "tfe_workspace_module" {
+  source  = "app.terraform.io/SpecialCustomer/tfe-workspace-module/specialcustomer"
+  version = "0.2.0"
+
+  enable_aws = "var.enable_aws"
+  enable_azure = "var.enable_azure"
+  enable_gcp = "var.enable_gcp"
+  vcs_repoidentifier = "${module.gitbucket_repo_module.repoidentifier}"
+  workspacename = "${var.projectname}-ws"
+}
---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable projectname {
+description="the name of the project"
+}
+
+variable enable_aws {
+description = "If this project need aws credentials"
+default=false
+}
+
+variable enable_azure {
+description = "If this project need Azure credentials"
+default=false
+}
+
+variable enable_gcp {
+description = "If this project need GCP credentials"
+default=false
+}
+
+
